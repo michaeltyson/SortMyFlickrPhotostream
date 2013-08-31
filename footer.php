f86d85f12e6ae506db7bdbfa48762936aa4d6a39
Michael Tyson <michael@atastypixel.com>
2013-08-31 16:24:40 +1000

    Altered download link to point to GitHub

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div class="footer">
     Copyright &copy; 2009 <a href="http://atastypixel.com/blog">Michael Tyson</a> &nbsp; | &nbsp; 
-    <a href="/download.php">Download the source for this application</a> &nbsp; | &nbsp;
+    <a href="https://github.com/michaeltyson/SortMyFlickrPhotostream">Download the source for this application</a> &nbsp; | &nbsp;
     <a href="/restore.php">Restore data from a backup</a> &nbsp; | &nbsp;
     <a href="http://atastypixel.com/blog/2009/06/08/sort-your-flickr-photostream/#respond">Comments?</a>
 </div>
